54e85874de0d003dba3b97b490e8ab82209528cf
hanquocadrian <hanquocadrian@gmail.com>
2021-06-11 00:09:52 +0700

    Adrian3l1 sync P1

--- api/DailyRate/dailyRate.controller.js
+++ api/DailyRate/dailyRate.controller.js
@@ -49,13 +49,20 @@ module.exports = {
         const data = req.body;
         var constraint = check(data);
         if(constraint !== true) return res.status(400).json(constraint);
+
         dailyRate.getDataByID(id, (err, result) => {
             if(err) { return res.status(500).json(err); }
             if(result.length <= 0) { return res.status(400).json('Record not exists to update!'); }
-            dailyRate.updateData(id, data, (err, result) => {
+
+            specialRate.getDataByThuNIDGTNPreventIdGTT(data.ngayBatDau, data.idLP, id, (err, result) => {
                 if(err) { return res.status(500).json(err); }
-                return res.status(200).json("Updated successfully");
-            })            
+                if(result.length > 0) { return res.status(400).json('This Rate for Special Rate is Exists!'); }
+            
+                dailyRate.updateData(id, data, (err, result) => {
+                    if(err) { return res.status(500).json(err); }
+                    return res.status(200).json("Updated successfully");
+                })         
+            }) 
         })
     },
     destroy: (req, res) => {
