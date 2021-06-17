ead68cc6aedb77aa712b16a99ea763fc919da40a
hanquocadrian <hanquocadrian@gmail.com>
2021-06-17 10:57:19 +0700

    sync P1

--- config/nodeMailer.config.js
+++ config/nodeMailer.config.js
@@ -4,7 +4,6 @@ const transport = nodemailer.createTransport({
     pool: true,
     host: "smtp.gmail.com",
     port: 587,
-    service: 'gmail',
     secure: false,
     auth: {
         user: "nativehotelct@gmail.com",
