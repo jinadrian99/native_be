59aee58863595e757822c0699934f49339b43085
hanquocadrian <hanquocadrian@gmail.com>
2021-06-17 11:09:20 +0700

    sync P1

--- config/nodeMailer.config.js
+++ config/nodeMailer.config.js
@@ -1,13 +1,19 @@
 const nodemailer = require('nodemailer');
 
 const transport = nodemailer.createTransport({
-    pool: true,
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    // pool: true,
+    // host: "smtp.gmail.com",
+    // port: 465,
+    // secure: true,
+    // auth: {
+    //     user: "nativehotelct@gmail.com",
+    //     pass: "Nh@123456"
+    // }
+    host: "smtp.mailtrap.io",
+    port: 2525,
     auth: {
-        user: "nativehotelct@gmail.com",
-        pass: "Nh@123456"
+      user: "d938ea764be572",
+      pass: "ab83feb3e1679f"
     }
 });
 
