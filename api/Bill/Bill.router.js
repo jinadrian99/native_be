21acb337fa2c729f3b2cf44bb962a24186c23314
jinadrian99 <hanquocadrian@gmail.com>
2021-07-03 15:52:18 +0700

    add code bill

--- api/Bill/Bill.router.js
+++ api/Bill/Bill.router.js
@@ -5,7 +5,7 @@ const BillController = require("./Bill.controller");
 router.get('/get-bills-by-id-customer-booking/:idKHD', BillController.getBillsByIDKHD);
 router.get('/change-status-to-deposit-by-id-bill/:idPTT', BillController.changeStatusToDepositByIdPTT);
 
-router.put('/:id', BillController.show);
+router.get('/:id', BillController.show);
 router.put('/:id', BillController.update);
 
 module.exports = router;
