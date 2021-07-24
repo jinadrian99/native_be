9bac685c61283fcf2a958659491811c2c5196eb8
jinadrian99 <hanquocadrian@gmail.com>
2021-07-24 14:57:48 +0700

    change: KHD, add code convert status 2: dps -> checkout

--- api/RoomRentalContract/RRC.router.js
+++ api/RoomRentalContract/RRC.router.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const RRCController = require("./RRC.controller");
 
 router.get('/get-by-idDDP/:id', RRCController.getRRCByIDDDP);
+router.get('/update-status-paid-by-idddp/:id', RRCController.updateStatusPaidByIDDDP);
 
 router.get('/', RRCController.index);
 router.get('/:id', RRCController.show);
